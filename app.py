f67177d532496378a35c3170d3e69b26203bd46c
Kenneth Rhee <kennethhrhee@gmail.com>
2016-11-12 03:28:49 -0800

    config and app changes

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = 5000
     app.run(host='0.0.0.0', port=port, debug=True)
--- config.py
+++ config.py
@@ -5,12 +5,12 @@ from flask import Flask
 app = Flask(__name__)
 
 try:
-	app.config.from_pyfile("dev_config.cfg")
+    app.config.from_pyfile("dev_config.cfg")
 
-	mc = MongoClient(app.config['MONGODB_URI'])
-	db = mc.passtheturing
+    mc = MongoClient(app.config['MONGODB_URI'])
+    db = mc.passtheturing
 
-	print(">>> Development configuration file loaded.")
+    print(">>> Development configuration file loaded.")
 except:
     #======================================
     # Try to get amazon ec2 container tags
