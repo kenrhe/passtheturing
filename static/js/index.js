1426f0ed6d9f27c741891b0c567d679f7077a30c
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2016-11-12 07:59:14 -0800

    Authenticate existence of chatbot

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,10 @@
+var chatbot = null;
+
 $(document).ready(function() {
-  $("#1,#2,#3,#4,#5,#6,#7").hide();
-  $("#1").delay(50).fadeIn(0);
-  $("#2").delay(100).fadeIn(0);
-  $("#3").delay(150).fadeIn(0);
-  $("#4").delay(800).fadeIn(0);
-  $("#5").delay(1200).fadeIn(0);
-  $("#6").delay(1250).fadeIn(0);
-  $("#7").delay(1300).fadeIn(0);
-  setTimeout(function() {
-    view.focusInput();
-  }, 1350);
+  view.focusInput();
   $("#input").on("keydown", function(event) {
     if (event.keyCode === 13) {
       event.preventDefault();
-      view.hideInput();
       controllers.submitQuery();
     }
   });
@@ -22,18 +13,44 @@ $(document).ready(function() {
 var controllers = {
   submitQuery: function() {
     var query = $("#input").text();
-    $.post({
-      url: '/submit',
-      data: query
-    }).done(function(data) {
-      if (data.success) {
-        view.addQueryAndResponse(query, data.response);
+    view.addQuery(query);
+    if (query.substring(0, 8) === "(chatbot") {
+      if (!chatbot) {
+        return this.loadChatbot(query.substring(9, query.length - 1));
       } else {
-        console.log(err);
+        return view.addSystemLine(0, "Need to quit current chatbot");
       }
-    }).fail(function(err) {
-      console.log(err);
-    });
+    } else if (query === "(quit)") {
+      if (chatbot) {
+        return this.quitChatbot();
+      } else {
+        return view.addSystemLine(0, "No current chatbot");
+      }
+    }
+    if (chatbot) {
+      $.post({
+        url: '/submit',
+        data: query
+      }).done(function(data) {
+        if (data.success) {
+          view.addResponse(data.response);
+        } else {
+          view.addSystemLine(0, data.response);
+        }
+      }).fail(function(err) {
+        console.log(err);
+      });
+    } else {
+      return view.addSystemLine(0, "No current chatbot");
+    }
+  },
+  loadChatbot: function(name) {
+    chatbot = name;
+    view.loadChatbot();
+  },
+  quitChatbot: function() {
+    chatbot = null;
+    view.quitChatbot();
   }
 };
 
@@ -41,24 +58,48 @@ var view = {
   scrollToBottom: function() {
     window.scrollTo(0, document.body.scrollHeight);
   },
-  showInput: function() {
-    $("#input").show();
-  },
-  hideInput: function() {
-    $("#input").hide();
-  },
   clearInput: function() {
     $("#input").text("");
   },
   focusInput: function() {
     $("#input").focus();
   },
-  addQueryAndResponse: function(query, response) {
-    $("#bash-output").append("<div class='bash-line'>user$ <span class='yellow'> " + query + "</span></div>\
-      <div class='bash-line'>chatbot$ <span class='green'> " + response + "</span></div>");
-    view.clearInput();
-    view.showInput();
-    view.focusInput();
-    view.scrollToBottom();
+  addUserLine: function(delay, message) {
+    setTimeout(function() {
+      $("#output").append("<div class='line'>user$ <span class='yellow'>" + message + "</span></div>");
+      view.scrollToBottom();
+    }, delay);
+  },
+  addChatbotLine: function(delay, message) {
+    setTimeout(function() {
+      $("#output").append("<div class='line'>" + chatbot + "$ <span class='green'>" + message + "</span></div>");
+      view.scrollToBottom();
+    }, delay);
+  },
+  addSystemLine: function(delay, message) {
+    setTimeout(function() {
+      $("#output").append("<div class='line'>system$ <span class='orange'>" + message + "</span></div>");
+      view.scrollToBottom();
+    }, delay);
+  },
+  addQuery: function(query) {
+    this.addUserLine(0, query);
+    this.clearInput();
+  },
+  addResponse: function(response) {
+    this.addChatbotLine(0, response);
+    this.focusInput();
+  },
+  loadChatbot: function() {
+    this.addSystemLine(50, "Loading modules.....");
+    this.addSystemLine(100, "Initializing synaptic network.....");
+    this.addSystemLine(1000, "Creating sandbox.....");
+    this.addSystemLine(1400, "Completed!");
+    this.addChatbotLine(1500, "Hi! Let's have a conversation!");
+  },
+  quitChatbot: function() {
+    this.addSystemLine(100, "Exiting sandbox.....");
+    this.addSystemLine(400, "Exiting synaptic network.....");
+    this.addSystemLine(1100, "Completed!");
   }
 };
