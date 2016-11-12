2995374dd1e75708f7d9be77e283ee1e38d621bb
Kenneth Rhee <kennethhrhee@gmail.com>
2016-11-12 01:15:19 -0800

    scrubbing

--- app.py
+++ app.py
@@ -3,15 +3,12 @@ Authored by: Kenneth Rhee, Hak Joon Lee, Joe Goodman
 Email: kennethhrhee@gmail.com, thejoonshow@gmail.com, goodman.a.joseph@gmail.com
 '''
 
-# from routes.browse import app_browse
-# from routes.search import app_search
 from flask import render_template, request, send_from_directory
-# from config import app, db
 from config import app
+
 import os
 
 # app.register_blueprint(app_browse)
-# app.register_blueprint(app_search)
 
 #===================================
 # Routes ####
@@ -19,14 +16,6 @@ import os
 @app.route('/')
 def index():
 	return render_template('index.html')
-
-@app.route('/cities')
-def cities():
-    return render_template('cities.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
--- push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git add --all
-echo "Please enter a comment:"
-read COMMENT
-git commit -m "$COMMENT"
-git push origin dev
