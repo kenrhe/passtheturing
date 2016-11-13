7802d2d29c99fd4ca306e15e20b9124a3da8ab67
Eliot Winchell <eliot.winchell@gmail.com>
2016-11-13 10:37:39 -0800

    Fix question asker

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 var chatbot = null;
 var chatbotResponseNumber = 0;
 var chatbotResponseIds = {};
-var startTime = null;
+var startTime = new Date();
 var asked = false;
 
 $(document).ready(function() {
@@ -27,10 +27,8 @@ var controllers = {
   askQuestion: function() {
     asked = true;
     $.getJSON('/question', {}, function(data) {
-      startTime = new Date();
-      asked = false;
       controllers.addId(data.id);
-      view.addResponse(data.response, !data.isDefault);
+      view.addResponse(data.response);
     });
   },
   submitQuery: function() {
@@ -38,53 +36,18 @@ var controllers = {
     asked = false;
     var query = $("#input").text();
     view.addQuery(query);
-<<<<<<< HEAD
-    // if (query.substring(0, 8) === "(chatbot") {
-    //   if (!chatbot) {
-    //     return this.loadChatbot(query.substring(9, query.length - 1));
-    //   } else {
-    //     return view.addSystemLine(0, "Need to quit current chatbot");
-    //   }
-    // } else if (query === "(quit)") {
-    //   if (chatbot) {
-    //     return this.quitChatbot();
-    //   } else {
-    //     return view.addSystemLine(0, "No current chatbot");
-    //   }
-    // }
-    // if (chatbot) {
-      view.addResponseProcessing();
-      $.getJSON('/submit', {
-        query: query
-      }, function(data) {
-        controllers.addId(data.id);
-        view.addResponse(data.response, !data.isDefault);
-      });
-    // } else {
-      // return view.addSystemLine(0, "No current chatbot");
-    // }
-  },
-  loadChatbot: function(name) {
-    chatbot = name;
-    view.loadChatbot();
-  },
-  quitChatbot: function() {
-    chatbot = null;
-    view.quitChatbot();
-=======
     view.addResponseProcessing();
     $.getJSON('/submit', {
       query: query
     }, function(data) {
       controllers.addId(data.id);
-      view.addResponse(data.response);      
+      view.addResponse(data.response);
     });
->>>>>>> 773ef084b782ebae8fa12ba5cbb590cc1d9510c4
   },
   addId: function(id) {
     chatbotResponseNumber++;
     chatbotResponseIds.chatbotResponseNumber = id;
-  },
+  }
 };
 
 var view = {
@@ -122,22 +85,12 @@ var view = {
       view.scrollToBottom();
     }, delay);
   },
-<<<<<<< HEAD
   addChatbotLine: function(delay, message, hasButtons) {
     setTimeout(function() {
       $("#output").append("<div class='line'>" + chatbot + "$ &zwnj;<span class='green'>" + message + "</span></div>");
       // if (hasButtons) {
       //   $("#output div:last").append("<div class='buttons'><i class='fa fa-thumbs-up' aria-hidden='true' id='up" + chatbotResponseNumber + "'></i><i class='fa fa-thumbs-down' aria-hidden='true' id='down" + chatbotResponseNumber + "'></i></div>");
       // }
-      view.scrollToBottom();
-    }, delay);
-  },
-  addSystemLine: function(delay, message) {
-=======
-  addChatbotLine: function(delay, message) {
->>>>>>> 773ef084b782ebae8fa12ba5cbb590cc1d9510c4
-    setTimeout(function() {
-      $("#output").append("<div class='line'>" + chatbot + "$ &zwnj;<span class='green'>" + message + "</span></div>");
       view.scrollToBottom();
     }, delay);
   },
@@ -150,54 +103,16 @@ var view = {
   },
   addResponse: function(response) {
     var length = response.length * 100;
-    var max = Math.max(3000, length);
     setTimeout(function() {
       $("#output div:last").remove();
     }, 1500);
     this.addChatbotLine(1500, "Alan is typing...");
     setTimeout(function() {
-<<<<<<< HEAD
       $("#output div:last").remove();
     }, Math.max(3000, length));
-    this.addChatbotLine(Math.max(3000, length), response, hasButtons);
+    this.addChatbotLine(Math.max(3000, length), response);
     setTimeout(function() {
       view.enableInput();
     }, Math.max(3000, length));
-  },
-  loadChatbot: function() {
-    this.hideInput();
-    this.addSystemLine(50, "Loading modules.....");
-    this.addSystemLine(100, "Initializing semantic network.....");
-    this.addSystemLine(1000, "Creating sandbox.....");
-    this.addSystemLine(1400, "Completed!");
-    this.addChatbotLine(1500, "Hey! How're you doing today?", false);
-    setTimeout(function() {
-      view.showInput();
-    }, 1500);
-  },
-  quitChatbot: function() {
-    this.hideInput();
-    this.addSystemLine(100, "Exiting sandbox.....");
-    this.addSystemLine(400, "Exiting semantic network.....");
-    this.addSystemLine(1100, "Completed!");
-    setTimeout(function() {
-      view.showInput();
-    }, 1100);
-  },
-  upvote: function(chatbotResponseNumber) {
-    $("#up" + chatbotResponseNumber).addClass("yellow");
-    $("#down" + chatbotResponseNumber).removeClass("yellow");
-  },
-  downvote: function(chatbotResponseNumber) {
-    $("#down" + chatbotResponseNumber).addClass("yellow");
-    $("#up" + chatbotResponseNumber).removeClass("yellow");
-=======
-      $("#output div:last").empty();
-    }, max);
-    this.addChatbotLine(max, response);
-    setTimeout(function() {
-      view.enableInput();
-    }, max);
->>>>>>> 773ef084b782ebae8fa12ba5cbb590cc1d9510c4
   }
 };
