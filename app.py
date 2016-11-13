aa900c28a21618dd4185b34e0e44d293b93cd07c
Kenneth Rhee <kennethhrhee@gmail.com>
2016-11-13 08:56:26 -0800

    Python Syntax and Structure Clean Up

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import render_template, request, send_from_directory, jsonify
 from config import app, db
-import twilio
-from twilio import twiml
+from ai import get_response
 
+import twilio
 import os
 import string
 import sys
@@ -12,57 +12,27 @@ import time
 # Routes ####
 #===================================
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/submit', methods=["GET"])
 def submit():
     query = str(request.args.get('query'))
 
-    resp = _submit(query)
+    resp = get_response(query)
     response = resp["response"]
     _id = resp["_id"]
     isDefault = resp["isDefault"]
 
     return jsonify(success=True, response=response, id=_id, isDefault=isDefault)
 
-def _submit(query):
-    query_clean = query.translate(None, string.punctuation).lower().strip()
-
-    a = db.dialogue.find_one({"query_clean": query_clean})
-    b = db.dialogue.find_one({"responses.0.2" : query_clean})
-
-    if a == None:
-        a = db.dialogue.find_one({"$text": {"$search": query_clean}})
-        if b:
-            response = b["query"]
-            id = b["query"]
-            isDefault=False
-
-        elif (a == None):
-            response = "What?"
-            response = db.dialogue.find_one({"query": b})
-        if a == None:
-            response = "What do you mean?"
-            id = None
-            isDefault = True
-
-        else:
-            response = a['responses'][0][0]
-            id = a['responses'][0][1]
-            isDefault = False
-
-    else:
-        response = a['responses'][0][0]
-        id = a['responses'][0][1]
-        isDefault = False
-
-    return {"_id" : id, "response" : response, "isDefault" : isDefault}
-
 @app.route('/sms/request', methods=["POST"])
 def sms_request():
     query = str(request.form['Body'])
-    sys.stderr.write(str(query))
-    resp = _submit(query)
+    resp = get_response(query)
 
-    response = twiml.Response()
+    response = twilio.twiml.Response()
     response.message(resp['response'])
 
     time.sleep(2)
@@ -77,30 +47,6 @@ def send_sms(to_number, body):
     client.messages.create(to_number,
                            from_=twilio_number,
                            body=body)    
-
-@app.route('/upvote', methods=["GET"])
-def upvote():
-    id = str(request.args.get('id'))
-    a = db.dialogue.find_one({"response.2": id})
-
-    if a:
-        a.update({}, {"$set": {"response.1": existing + 1}})
-
-    return jsonify(success=True)
-
-@app.route('/downvote', methods=["GET"])
-def downvote():
-    id = str(request.args.get('id'))
-    a = db.dialogue.find_one({"response.1": id})
-
-    if a:
-        a.update({}, {"$set": {"response.1": existing - 1}})
-
-    return jsonify(success=True)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
